45f821b7e58b78cca3337f6def914a627b8c5a41
Humberto Tejada <humtej1204@gmail.com>
2022-05-12 20:44:51 -0500

    Correcting puppet lint

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,7 +4,7 @@ package { 'nginx':
 }
 exec { '':
   provider => shell,
-  command => ['sudo chown -R ubuntu /var/www',
+  command  => ['sudo chown -R ubuntu /var/www',
               'echo "Hellow World" > /var/www/html/index.nginx-debian.html',
               'sudo sed -i "53i\ \n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=_WRBAzV-iHI&t=1s}\n" /etc/nginx/sites-available/default',
               'sudo service nginx restart'],
