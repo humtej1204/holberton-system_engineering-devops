05ea12c5bfe4aed6e916018dcd41bcb1425ee2b5
Humberto Tejada <humtej1204@gmail.com>
2022-05-12 20:42:46 -0500

    Correting exec command

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,7 +5,7 @@ package { 'nginx':
 exec { '':
   provider => shell,
   command => ['sudo chown -R ubuntu /var/www',
-              'echo "Hello World" > /var/www/html/index.nginx-debian.html',
+              'echo "Hellow World" > /var/www/html/index.nginx-debian.html',
               'sudo sed -i "53i\ \n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=_WRBAzV-iHI&t=1s}\n" /etc/nginx/sites-available/default',
               'sudo service nginx restart'],
 }
