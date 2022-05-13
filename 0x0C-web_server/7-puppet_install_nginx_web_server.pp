ab72393e4d89daaf6bfd26b12bf0d2f267d3ddae
Humberto Tejada <humtej1204@gmail.com>
2022-05-12 20:59:01 -0500

    test

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,7 @@
 # Install and configure an Nginx server
 exec { 'configuration':
   provider => shell,
-  command  => ['sudo apt-get update', 'sudo apt install nginx -y',
+  command  => ['sudo apt-get -y update', 'sudo apt-get -y install nginx',
               'sudo chown -R ubuntu /var/www',
               'echo "Hellow World" > /var/www/html/index.nginx-debian.html',
               'sudo sed -i "53i\ \n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=_WRBAzV-iHI&t=1s}\n" /etc/nginx/sites-available/default',
