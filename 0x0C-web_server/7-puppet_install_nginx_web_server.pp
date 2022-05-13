6f6efeeb1581f4f88c197af3a90d3ebe6ad40e48
Humberto Tejada <humtej1204@gmail.com>
2022-05-12 20:53:58 -0500

    deleting package command

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,11 +1,8 @@
 # Install and configure an Nginx server
-package { 'nginx':
- ensure  => installed,
- command => 'sudo apt install nginx -y',
-}
 exec { 'configuration':
   provider => shell,
-  command  => ['sudo chown -R ubuntu /var/www',
+  command  => ['sudo apt-get update', 'sudo apt install nginx -y',
+              'sudo chown -R ubuntu /var/www',
               'echo "Hellow World" > /var/www/html/index.nginx-debian.html',
               'sudo sed -i "53i\ \n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=_WRBAzV-iHI&t=1s}\n" /etc/nginx/sites-available/default',
               'sudo service nginx restart'],
