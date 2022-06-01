819a5abbab7e275225b52c3133bdab9640c015eb
Humberto Tejada <humtej1204@gmail.com>
2022-06-01 00:04:48 -0500

    Writting code

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit.
+If no results are found for the given subreddit,
+the function should return None.
+'''
+
+import requests
+
+'''Data'''
+header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+          AppleWebKit/537.36 (KHTML, like Gecko) \
+          Chrome/70.0.3538.77 Safari/537.36"}
+
+
+def recurse(subreddit, hot_list=[]):
+    url = "https://www.reddit.com/r/" + str(subreddit) + "/hot.json"
+    res = requests.get(url, headers=header)
+
+    if res.status_code == 200:
+        res = res.json()
+        after = res.get('data').get('after')
+        hots = res.get('data').get('children')
+        hot_list += list(map(lambda elm: elm.get('data').get('title'), hots))
+        return recurse(subreddit, hot_list)
+    return None
