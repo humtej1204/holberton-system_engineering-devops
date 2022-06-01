0e99cf83c6abbcac16861081df005ff3cda807e5
Humberto Tejada <humtej1204@gmail.com>
2022-06-01 02:10:37 -0500

    Correcting code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,11 +8,16 @@ the function should return None.
 
 import requests
 
+'''Data'''
+header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+          AppleWebKit/537.36 (KHTML, like Gecko) \
+          Chrome/70.0.3538.77 Safari/537.36"}
+
 
 def recurse(subreddit, hot_list=[], after=""):
     """List containing the titles of all hot articles for a given subreddit"""
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    res = requests.get(url, headers={'User-Agent': 'AngentMEGO'},
+    url = "https://www.reddit.com/r/" + str(subreddit) + "/hot.json"
+    res = requests.get(url, headers=header,
                        params={'after': after})
 
     if after is None:
@@ -22,6 +27,8 @@ def recurse(subreddit, hot_list=[], after=""):
         res = res.json()
         after = res.get('data').get('after')
         hots = res.get('data').get('children')
-        hot_list += list(map(lambda elm: elm.get('data').get('title'), hots))
+        #hot_list += list(map(lambda elm: elm.get('data').get('title'), hots))
+        for l in hots:
+            hot_list.append(l.get('data').get('title'))
         return recurse(subreddit, hot_list, after)
     return None
