0c5fb8c5579dfa936b0cc24bebd3a93af167a75d
Humberto Tejada <humtej1204@gmail.com>
2022-06-01 21:17:07 -0500

    Correcting puppet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 file_line { 'fix Apache error':
   ensure => 'present',
   path   => '/var/www/html/wp-settings.php',
-  line   => 'require( ABSPATH . WPINC . '/class-wp-error.php' );',
-  match  => 'require( ABSPATH . WPINC . '/class-wp-error.phpp' );'
+  line   => "require( ABSPATH . WPINC . '/class-wp-error.php' );",
+  match  => "require( ABSPATH . WPINC . '/class-wp-error.phpp' );",
 }
