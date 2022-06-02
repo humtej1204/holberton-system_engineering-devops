78a691b362bb4d66cda1810befc977c27687ca2a
Humberto Tejada <humtej1204@gmail.com>
2022-06-01 21:33:26 -0500

    Correcting code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix error found, Apache is returning a 500 error.
 exec { 'fixing error':
-  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/',
 }
