4494b14aab6c26b6ba7b95781b3973dcec450aab
Humberto Tejada <humtej1204@gmail.com>
2022-06-01 21:06:40 -0500

    Creating file and writting code

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fix error found, Apache is returning a 500 error.
+file_line { 'fix Apache error':
+  ensure => 'present',
+  path   => '/var/www/html/wp-settings.php',
+  line   => 'require( ABSPATH . WPINC . '/class-wp-error.php' );',
+  match  => 'require( ABSPATH . WPINC . '/class-wp-error.phpp' );'
+}
